9e286e764a8fe89291ff64906626d3a4db3d2f09
ChaseSivuku <113906831+ChaseSivuku@users.noreply.github.com>
2025-07-24 08:23:58 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+name = input("Please enter your first name ")
+surname = input("Please enter your surname ")
+
+
+
+print("Hello ",name.capitalize(),surname.capitalize(),"I hope you have a wonderful day!")
